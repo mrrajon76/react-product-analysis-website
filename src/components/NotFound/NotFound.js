e30fcd34014edfedbaba599d44dc9b56d66bf223
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-04-05 03:13:32 +0600

    404 page added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notfound from '../../images/404-error.gif';
 
 const NotFound = () => {
     return (
-        <div>
-
+        <div className='px-28 py-5 bg-slate-100'>
+            <Link to="/">
+                <img src={notfound} alt="" className='block mx-auto' />
+            </Link>
         </div>
     );
 };
